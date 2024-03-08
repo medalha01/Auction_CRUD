d3527d6d26d75676e0fb5d7c9e55487b187c6b68
Isac Martins <isacmartins.223@gmail.com>
2024-03-08 20:05:40 -0300

    Feat: Deploying back-end to vercel

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,11 @@ async function bootstrap() {
     .setTitle('Leilão API')
     .setDescription('API de Leilão de Carros')
     .setVersion('1.0')
+    .addTag('leilao')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(4000);
+  await app.listen(3000);
 }
 bootstrap();
